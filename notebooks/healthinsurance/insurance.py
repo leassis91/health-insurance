6cd8dba886485cc5c5b60de28f08a150bdf8ba40
Leandro Destefani <leassis.destefani@gmail.com>
2022-09-29 15:51:24 -0300

    Add files via upload

---
+++ notebooks/healthinsurance/insurance.py
@@ -0,0 +1,72 @@
+import pickle
+import pandas as pd
+import numpy as np
+
+class HealthInsurance:
+    def __init__(self):
+        # self.home_path = ''
+        self.annual_premium_scaler            = pickle.load(open('features/annual_premium_scaler.pkl', 'rb'))
+        self.age_scaler                       = pickle.load(open('features/age_scaler.pkl', 'rb'))
+        self.vintage_scaler                   = pickle.load(open('features/vintage_scaler.pkl', 'rb'))
+        self.target_encode_region_code_scaler = pickle.load(open('features/target_encode_region_code_scaler.pkl', 'rb'))
+        self.fe_policy_sales_channel_scaler   = pickle.load(open('features/fe_policy_sales_channel.pkl', 'rb'))
+        
+    def data_cleaning(self, df):
+        cols_new = ['id','gender','age','driving_license','region_code','previously_insured','vehicle_age','vehicle_damage','annual_premium','policy_sales_channel','vintage']
+        
+        # rename
+        df.columns = cols_new
+        
+        return df
+        
+    def feature_engineering(self, df):
+        # Vehicle Damage Number
+        df['vehicle_damage'] = df['vehicle_damage'].apply(lambda x: 1 if x == 'Yes' else 0)
+        
+        # Vehicle Age
+        df['vehicle_age'] = df['vehicle_age'].apply(lambda x: 'over_2_years' if x == '> 2 Years' else 'between_1-2_years'
+                                                                             if x == '1-2 Year' else 'below_1_year')
+        
+        return df
+    
+    def data_preparation(self, df):        
+        
+        ## annual_premium
+        df['annual_premium'] = self.annual_premium_scaler.transform(df[['annual_premium']].values)
+        
+        # age
+        df['age'] = self.age_scaler.transform(df[['age']].values)
+
+        # vintage
+        df['vintage'] = self.vintage_scaler.transform(df[['vintage']].values)
+
+        # gender
+        df = pd.get_dummies(df,prefix='gender',columns=['gender'])
+        
+        # region_code - Frequency Encoding / Target Encoding / Weighted Target Encoding
+        df.loc[:, 'region_code'] = df['region_code'].map(self.target_encode_region_code_scaler)
+
+        # vehicle_age - One Hot Encoding / Frequency Encoding / Order Encoding
+        df = pd.get_dummies(df, prefix='vehicle_age', columns=['vehicle_age'])
+
+        # policy_sales_channel - Target Encoding / Frequency Encoding
+        df.loc[:, 'policy_sales_channel'] = df['policy_sales_channel'].map(self.fe_policy_sales_channel_scaler)
+
+
+        # Feature Selection
+        cols_selected = ['annual_premium', 'vintage', 'age', 'region_code', 'vehicle_damage', 
+                         'previously_insured', 'policy_sales_channel']
+        
+        df = df.fillna(0)
+
+        return df[cols_selected]
+    
+    def get_prediction(self, model, original_data, test_data):
+        # model prediction
+        pred = model.predict_proba(test_data)
+            
+        # join prediction
+        original_data['prediction'] = pred[:, 1]
+        original_data = original_data.sort_values('prediction', ascending=False)
+        
+        return original_data.to_json(orient='records', date_format='iso')
---
+++ webapp/handler.py
@@ -0,0 +1,50 @@
+import os
+import pickle
+import pandas as pd
+from flask import Flask, request, Response
+# from NOME_DA_PASTA.NOME_DO_ARQUIVO import NOME_DA_FUNCAO
+from healthinsurance.insurance import HealthInsurance
+
+
+# loading model
+# path = 'home/leassis/repos_wsl/projects/health_insurance/'
+model = pickle.load(open('models/lgbm_final.pkl', 'rb'))
+
+# initialize API
+app = Flask(__name__)
+
+@app.route('/healthinsurance/predict', methods=['POST'])
+def health_insurance_predict():
+    test_json = request.get_json()
+    
+    if test_json: #verifica se existe
+        
+        if isinstance(test_json, dict):
+            test_raw = pd.DataFrame(test_json, index=[0])
+            
+        else:
+            test_json = pd.DataFrame(test_json, columns=test_json[0].keys())
+            
+        pipeline = HealthInsurance()
+
+        # data cleaning
+        df1 = pipeline.data_cleaning(test_raw)
+
+        # feature engineering
+        df2 = pipeline.feature_engineering(df1)
+
+        # data preparation
+        df3 = pipeline.data_preparation(df2)
+
+        # prediction
+        df_response = pipeline.get_prediction(model, test_raw, df3)
+
+
+        return df_response
+    
+    else:
+        return Response('{}', status=200, mimetype='application/json')
+    
+if __name__ == '__main__':
+    port = os.environ.get('PORT', 5000)
+    app.run('0.0.0.0', port=port)
